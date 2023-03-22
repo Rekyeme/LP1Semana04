1b2b70029fbbaf24bface00be7ddc63384134764
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-22 20:27:06 +0000

    Ex.5 grammar corrections. Added comments in XML format to the 3 methods

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,11 +4,19 @@ namespace Power2Method
 {
     class Program
     {
-        private static void Power2OfUntil5M()
+        /// <summary>
+        /// Call for the method PowerOf2UntilN to print the powers of 2 from 2 to 32;
+        /// </summary>
+        private static void Power2OfUntil5()
         {
            PowerOf2UntilN(5);
         }
 
+
+        /// <summary>
+        /// Prints the powers from of 2 from 1 to n using a for loop; 
+        /// </summary>
+        /// <param name="args"></param>
         private static void PowerOf2UntilN(int n)
         {
             for(int i = 1; i <= (1 << n); i = i << 1)
@@ -16,9 +24,17 @@ namespace Power2Method
                 Console.WriteLine(i);
             }
         }
+
+        
+        /// <summary>
+        /// Summons the method Power2OfUntil5 to print the powers of 2 from 1 to 32. 
+        /// After that summons the methods PowerOf2UntilN(6) and PowerOf2UntilN(12) and print
+        ///the powers of 2 from 1 to 64 and 1 to 4096.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Power2OfUntil5M();
+            Power2OfUntil5();
             PowerOf2UntilN(6);
             PowerOf2UntilN(12);
         }
