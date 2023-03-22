9fd2b4f50c9cd63a288666d5a08a23c8d942ae87
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-22 10:36:00 +0000

    Method Creation

---
+++ Power2Method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Power2Method
+{
+    class Program
+    {
+        private static void Power2Until5M()
+        {
+            for(int i = 1; i <= (1 << 5); i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    
+        static void Main(string[] args)
+        {
+        
+        
+        }
+    }
+}
